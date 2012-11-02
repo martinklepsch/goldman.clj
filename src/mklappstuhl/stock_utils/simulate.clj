763b252d2d1f84ee7fe281c9d586a6094d17a1e7
Martin Klepsch <martinklepsch@gmail.com>
2012-11-02 01:35:11 +0100

    add test fund and some refactoring

--- src/mklappstuhl/stock_utils/simulate.clj
+++ src/mklappstuhl/stock_utils/simulate.clj
@@ -1,7 +1,15 @@
-(ns mklappstuhl.stock-utils.simulate)
+(ns mklappstuhl.stock-utils.simulate
+  (:require [clj-time.core :as time]))
 
-(defn buy [amount etf date]
+(defn buy [etf amount date]
   {:etf etf :amount amount :date date})
 
 (defn fund [name & trades]
   {name trades})
+
+(defn testfund []
+  (fund :compinvest1
+        (buy "AAPL" 100000 (time/date-time 2011 1 1))
+        (buy "IBM"  400000 (time/date-time 2011 1 1))
+        (buy "GOOG" 200000 (time/date-time 2011 1 1))
+        (buy "MSFT" 300000 (time/date-time 2011 1 1))))
