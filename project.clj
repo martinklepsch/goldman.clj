758b37b1633533f69f95cbd42ae70a0b940ff1cc
Martin Klepsch <martinklepsch@gmail.com>
2012-11-02 01:32:19 +0100

    adds incanter for things like stdev

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "analyze stock data by common metrics"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [yfinance "0.2.0"]
+                 [incanter  "1.3.0"]
                  [org.clojars.bmabey/csvlib "0.3.4"]
                  [org.clojars.jwhitlark/clj-time "0.4.5-SNAPSHOT"]])
