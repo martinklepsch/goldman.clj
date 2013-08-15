7ea10ce1997a897581bef5018b807f01981e5ae3
Martin Klepsch <martinklepsch@gmail.com>
2013-08-15 12:24:33 +0200

    add korma as dependency

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [yfinance "0.2.0"]
                  [incanter  "1.3.0"]
+                 [korma "0.3.0-RC5"]
                  [org.clojars.bmabey/csvlib "0.3.4"]
                  [org.clojars.jwhitlark/clj-time "0.4.5-SNAPSHOT"]])
