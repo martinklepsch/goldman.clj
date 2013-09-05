ece1dfe2ede8df4ebb274c5a898bd091ddd9450b
Martin Klepsch <martinklepsch@gmail.com>
2013-09-05 21:15:28 +0200

    add some lightweight migration framework into the db namespace

--- src/mklappstuhl/stock_utils/db.clj
+++ src/mklappstuhl/stock_utils/db.clj
@@ -1,54 +1,73 @@
 (ns mklappstuhl.stock-utils.db
   (:require [korma.db :as kdb]
             [korma.core :as k]
-            [clojure.java.jdbc :as jdbc]))
+            [clojure.java.jdbc :as sql]))
 
 
-(def pg (kdb/postgres {:db "goldman"
+(def db (kdb/postgres {:db "goldman"
                       :user "goldman"
                       :password ""}))
 
-(defn create-stock_symbols
-  "creates the table stocks in the database"
-  []
-  (jdbc/create-table
-   :stock_symbols
-   [:id :serial "PRIMARY KEY"]
-   [:name "varchar(15)"]  ;; longest stock-name is 5 characters long
-   [:full_name "varchar(255)"]
-   [:sector "varchar(255)"]
-   [:industry "varchar(255)"]
-   ))
-
-; should be called trading-days?
-(defn create-stocks
-  "creates the table stocks in the database"
-  []
-  (jdbc/create-table
-   :stock
-   [:id :serial "PRIMARY KEY"]
-   ; maybe that field should be just called date?
-   ; also it should just be a date and no hours, minutes, whatsoever
-   ; and it should be unique
-   [:trading_date :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"] ;d2
-   ;; TODO: what do we want in the db? ask/bid/volume...
-   [:stock_symbol_id :serial "references stock_symbol (id)"] ;; foreign key ;s
-   [:high :integer "NOT NULL"] ;h
-   [:low :integer "NOT NULL"] ;g
-   [:open :integer "NOT NULL"] ;o
-   [:close :integer "NOT NULL"] ; previous day - p
-   [:volume :integer "NOT NULL"] ;v
-   [:ask :integer "NOT NULL"] ;a
-   [:bid :integer "NOT NULL"] ;b
-   ))
-
 (defn persist-day [sym day-data]
-  (jdbc/insert! pg :stock day-data))
+  (sql/insert! db :stock day-data))
 
-(defn create-tables!
-  "creates the necessary tables (stock,stock_symbol)"
-  []
- (jdbc/with-connection pg
-   (jdbc/transaction
-     (create-stock_symbols)
-     (create-stocks))))
+(defn drop-schema []
+  (map sql/drop-table '(:symbols :market-days)))
+
+(defn initial-schema []
+  (sql/create-table
+    :symbols
+    [:id :serial "PRIMARY KEY"]
+    [:name "varchar(15)" "UNIQUE"]  ;; longest stock-name is 5 characters long
+    [:full_name "varchar(255)"]
+    [:sector "varchar(255)"]
+    [:industry "varchar(255)"])
+
+  (sql/create-table
+    :market-days
+    [:id :serial "PRIMARY KEY"]
+    [:date :date "UNIQUE NOT NULL" "DEFAULT CURRENT_DATE"] ;d2
+    ;; TODO: what do we want in the db? ask/bid/volume...
+    [:stock_symbol_id :serial "references symbols (id)"] ;; foreign key ;s
+    [:high :integer "NOT NULL"] ;h
+    [:low :integer "NOT NULL"] ;g
+    [:open :integer "NOT NULL"] ;o
+    [:close :integer "NOT NULL"] ; previous day - p
+    [:volume :integer "NOT NULL"] ;v
+    [:ask :integer "NOT NULL"] ;a
+    [:bid :integer "NOT NULL"])) ;b
+
+; this structure can be used to alter the current db schema
+; (defn add-whatever []
+;     (sql/do-commands "ALTER TABLE symbols ADD COLUMN whatever VARCHAR"))
+
+
+; I basically took this from the link below. It's somehow a super lightweight
+; migration system. Let's see how long this works. I didn't understand
+; everything of this to be honest.
+; https://github.com/technomancy/syme/blob/master/src/syme/db.clj#L99-L125
+(defn run-and-record [migration]
+  (println "Running migration:" (:name (meta migration)))
+  (migration)
+  (sql/insert-values "migrations" [:name :created_at]
+                     [(str (:name (meta migration)))
+                      (java.sql.Timestamp. (System/currentTimeMillis))]))
+
+(defn migrate [& migrations]
+  (sql/with-connection db
+    (try (sql/create-table "migrations"
+                          [:name :varchar "NOT NULL"]
+                          [:created_at :timestamp "NOT NULL"  "DEFAULT CURRENT_TIMESTAMP"])
+         (catch Exception _))
+    (sql/transaction
+      (let [has-run? (sql/with-query-results run ["SELECT name FROM migrations"]
+                       (set (map :name run)))]
+        (doseq [m migrations 
+                :when (not (has-run? (str (:name (meta m)))))]
+          (run-and-record m))))))
+
+(defn migrate-all []
+  "run all migrations"
+  (migrate #'initial-schema
+           ; #'add-whatever
+           ))
