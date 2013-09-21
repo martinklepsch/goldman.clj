767b9d3c63a657a47bb165238468a87753fe24ed
Martin Klepsch <martinklepsch@gmail.com>
2013-09-21 05:13:32 +0200

    trying to figure out where the error when saving days is occurring now

--- src/mklappstuhl/stock_utils/data.clj
+++ src/mklappstuhl/stock_utils/data.clj
@@ -1,5 +1,7 @@
 (ns mklappstuhl.stock-utils.data
   (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.pprint :as pp]
             [mklappstuhl.stock-utils.util :as util]
             [mklappstuhl.stock-utils.db :as db]
             [in.freegeek.yfinance :as yfinance]
@@ -7,24 +9,26 @@
             [clojure.data.csv :as csv]))
 
 
-(def companies ["AAPL" "GOOG" "IBM" "CCL" "ADBE" "AMZN" "CSCO" "EBAY" "INTC"])
-
 (defn parse-day [line]
   "takes a line from Yahoo! Finance CSV data and returns
   a map where the keys are the column names"
-  (zipmap '(:date :open :high :low :close :volume :adj-close)
+  (println line)
+  (zipmap '(:trading_date :open :high :low :close :volume :adjusted_close)
            (cons (util/parse-date (first line))
                  (map read-string (rest line)))))
 
 (defn parse-response [resp]
   "takes the response from a yfinance query and parses it to a li"
-  (let [records (csv/read-csv resp)]
-    (map parse-day (rest records))))
+  ; (let [records (csv/read-csv resp)]
+    (println resp)
+    (map parse-day (rest records)))
 
-(defn save-trading-data [sym]
+(defn sync-trading-data [stock]
   "load Yahoo! Finance data for given symbol and save it to data/ directory"
-  (let [data (get (yfinance/fetch-historical-data "2011-12-01" "2011-12-15" [sym]) sym)]
-    (map db/persist-day sym (parse-response data))))
+  (let [id (:id stock)
+        sym (:name stock)
+        data (yfinance/fetch-historical-data "2011-12-09" "2011-12-15" [sym])]
+    (parse-response data)))
 
 (defn load-trading-data [trade]
   ; Rewrite this function so that it downloads data
