cf783347df4f5d2fde069a919152541022b3a9fd
Martin Klepsch <martinklepsch@gmail.com>
2013-09-21 17:34:23 +0200

    push for debugging

--- src/mklappstuhl/stock_utils/data.clj
+++ src/mklappstuhl/stock_utils/data.clj
@@ -9,26 +9,25 @@
             [clojure.data.csv :as csv]))
 
 
-(defn parse-day [line]
-  "takes a line from Yahoo! Finance CSV data and returns
-  a map where the keys are the column names"
-  (println line)
-  (zipmap '(:trading_date :open :high :low :close :volume :adjusted_close)
-           (cons (util/parse-date (first line))
-                 (map read-string (rest line)))))
+; (defn parse-day [line]
+;   "takes a line from Yahoo! Finance CSV data and returns
+;   a map where the keys are the column names"
+;   (zipmap '(:trading_date :open :high :low :close :volume :adjusted_close)
+;            (cons (util/parse-date (first line))
+;                  (map read-string (rest line)))))
 
-(defn parse-response [resp]
-  "takes the response from a yfinance query and parses it to a li"
-  ; (let [records (csv/read-csv resp)]
-    (println resp)
-    (map parse-day (rest records)))
+; (defn parse-response [resp]
+;   "takes the response from a yfinance query and parses it to a li"
+;   (let [records (csv/read-csv resp)]
+;     (map parse-day (rest records))))
 
 (defn sync-trading-data [stock]
   "load Yahoo! Finance data for given symbol and save it to data/ directory"
   (let [id (:id stock)
         sym (:name stock)
-        data (yfinance/fetch-historical-data "2011-12-09" "2011-12-15" [sym])]
-    (parse-response data)))
+        data (get (yfinance/fetch-historical-data "2011-12-09" "2011-12-15" [sym]) sym)]
+    (println id)))
+    ; (map (partial db/persist-day id) (parse-response data))))
 
 (defn load-trading-data [trade]
   ; Rewrite this function so that it downloads data
