d12362fa029cfa9982bc44af8e360883828f88fd
TN1ck <Tom.w.a.nick@gmail.com>
2013-09-22 12:46:33 +0200

    changed to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject stock-utils "0.0.1"
   :description "analyze stock data by common metrics"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [yfinance "0.2.0"]
                  [incanter  "1.3.0"]
                  [korma "0.3.0-RC5"]
