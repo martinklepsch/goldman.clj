02165dac0aef6f809cc377ed305a070aac3ee722
TN1ck <tomwanick@gmail.com>
2013-10-27 16:41:11 +0100

    added nvd3 serializer function

--- src/mklappstuhl/stock_utils/metrics.clj
+++ src/mklappstuhl/stock_utils/metrics.clj
@@ -1,6 +1,7 @@
 (ns mklappstuhl.stock-utils.metrics
   (:require [mklappstuhl.stock-utils.populate :as populate]
             [mklappstuhl.stock-utils.persistence :as pers]
+            [clj-time.coerce :as coerce]
             [korma.core :as k]
             [incanter.stats :as stats]))
 
@@ -30,6 +31,17 @@
     (* k (/ (mean-daily-returns trade)
             (stats/sd (vals (daily-returns trade)))))))
 
+(defn stock-data->nvd3 [days]
+  "takes a days-map and returns and equivalent of a nvd3 datastructure
+  [{:stock-name 'AABB', :bid 0.5, :trading-date '2013-...' ... }, {..}, ...] ->
+  [{:key 'AABB-bid', :values [['2013-..' 0.5]]}]"
+  (let [stock-name (:stock_name (first days))
+        x-key :trading_date
+        y-keys [:adjusted_close :low :high :ask :bid]]
+    (map (fn [y-key] {:key (str stock-name "-" (name y-key))
+                      :values (map (juxt (comp coerce/to-long x-key) y-key) days)})
+         y-keys)))
+
 (defn get-stock-data [stock]
   (k/select pers/days
             (k/where {:stock_name (name stock)})))
@@ -37,3 +49,5 @@
   (k/select pers/stocks))
 
 ; (get-stock-data :AABB)
+
+
